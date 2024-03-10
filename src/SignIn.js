248950efcdc0c1d9f32af8f9a721c071a4ed7b5f
Gravit09 <garvitchoudhary09@gmail.com>
2024-03-10 22:53:45 +0530

    fix1

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { auth } from "./firebase";
+import { Link } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 export default function SignIn() {
   const [email, setEmail] = useState("");
@@ -38,14 +39,16 @@ export default function SignIn() {
       </div>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="./img/netflix (1).png"
-            alt="NoteFlix"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to Upload Dashboard
-          </h2>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <img
+              className="mx-auto h-10 w-auto"
+              src="./img/netflix (1).png"
+              alt="NoteFlix"
+            />
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign in to Upload Dashboard
+            </h2>
+          </Link>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
